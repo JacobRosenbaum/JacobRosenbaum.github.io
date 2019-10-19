af6939488424871623962fc43ecb06d459df77e6
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2019-10-19 12:32:12 -0500

    still need to figure out how to use moment to input times, and fix some bugs in my local storage function

--- homework5/planner.js
+++ homework5/planner.js
@@ -1,32 +1,32 @@
-var now = moment().subtract(10, 'days').calendar();
+var currentTime = moment().format();
+console.log(currentTime);
+var now = moment().format('MMMM Do, YYYY');
 var moment = $("#displayMoment");
 moment.text(now);
-moment.css("text-align","center");
-// var currentTime = moment().format();    
-// console.log(currentTime);
- var result = $("#exampleFormControlTextarea1");
- var lock = $(".lock");
+moment.css("text-align", "center");
+var result = $(".form-control");
+var lock = $(".lock");
 
-// if (planner.input === ""){
-//     displayMessage("error","Cannot save if you don't write anything");
-// }
-// else{
-//     displayMessage("success","Registered successfully");
-// }
-
- $(lock).on("click", function(store){
-     store.preventDefault();
-     console.log(result.val());
-     var planner = {
-        input: result.val(),
+$(lock).on("click", function(store) {
+    store.preventDefault();
+    console.log($("#text" + result).val());
+    var planner = {
+        input: result.$(this).attr("value"),
     };
-     localStorage.setItem("planner", JSON.stringify(planner));
-     
-    console.log(lastUser.input);
- });
-
- $( document ).ready(function() {
+    if (planner.input === "") {
+        alert("Error - Cannot save if you don't write anything");
+    } else {
+        localStorage.setItem("planner", JSON.stringify(planner));
+    }
+});
+$(window).on("load", function() {
     var lastUser = JSON.parse(localStorage.getItem("planner"));
-    result.textContent = lastUser.input;
+    result.text(lastUser.input);
+    console.log(lastUser.input);
 });
 
+$(window).on("load", function() {
+    if ($(".col-lg-2") < currentTime) {
+        $(".col-lg-2").css("background-color", "red");
+    }
+});
