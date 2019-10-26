098c50d05c6f5d5a0dafdcd1feb816e70ab02ff5
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2019-10-26 14:00:12 -0500

    MVP is working

--- homework6/logic.js
+++ homework6/logic.js
@@ -1,19 +1,66 @@
-var apiKey = e84c468c18a9f7388a0eb7ae72214832;
-var cityValue = $("#inputValue").trim().val();
+var apiKey = "e84c468c18a9f7388a0eb7ae72214832";
+
 var now = moment().format('MMMM Do, YYYY');
 
+function getWeather() {
+    event.preventDefault();
+    var cityValue = $("#inputValue").val().trim();
+    var queryURL = "http://api.openweathermap.org/data/2.5/find?q=" + cityValue + "&units=imperial&type=accurate&mode=json&APPID=" + apiKey;
+    console.log(queryURL);
 
-$("#searchButton").on("click", function() {
-    var queryURL = "https://.openweathermap.org/data/2.5/weather?q=" + cityValue + "&apikey=" + apiKey;
     $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function(response) {
-            $("#cityName").text(response.name + " " + now);
-            $("#temp").text("Temperature: " + response.main.temp + " F");
-            $("#humidity").text("Humidity: " + response.main.humidity + " %");
-            $("#wind").text("Wind Speed " + response.wind.speed + " MPH");
-            $("#searchDiv").prepend(response.name);
-        });
+        url: queryURL,
+        type: "GET",
+    }).then(function(response) {
+        console.log(response);
+        $("#cityName").html("<h1>" + response.list[0].name + " " + now + "</h1>");
+        $("#temp").html("<h2>" + "Temperature: " + response.list[1].main.temp + " F" + "</h2>");
+        $("#humidity").html("<h2>" + "Humidity: " + response.list[1].main.humidity + " %" + "</h2>");
+        $("#wind").html("<h2>" + "Wind Speed " + response.list[2].wind.speed + " MPH" + "</h2>");
+        $("#searchDiv").text(response.list[0].name);
+
+        console.log("Temperature: " + response.list[1].main.temp + " F");
+        console.log("Humidity: " + response.list[1].humidity + " %");
+        console.log("Wind Speed " + response.list[2].wind.speed + " MPH");
+        console.log(response.list[0].name);
+        localStorage.setItem("city", response.list[0].name);
+
+    });
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityValue + "&units=imperial&type=accurate&mode=json&APPID=" + apiKey;
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        type: "GET",
+    }).then(function(response) {
+        console.log(response);
+        $("#forecast1").empty();
+        for (var i = 0; i < 5; i++) {
+            console.log(response.list[i].main.temp);
+            var newCard = $("<div class='days'>");
+            newCard.html(`
+            <div> Temperature: ${response.list[i].main.temp}&deg;F </div>
+            <div> Humidity: ${response.list[i].main.humidity}% </div>
+            `)
+            $("#forecast1").append(newCard);
+
+        }
+    });
+}
+
+
+$("#searchButton").on("click", function(event) {
+
+    getWeather();
+
+
 });
+$(window).on("load", function() {
+    var newCity = localStorage.getItem("city");
+    $("#searchDiv").prepend(newCity);
+    $("#inputValue").val(newCity);
+});
+$(newCity).on("click", function() {
+    getWeather();
+})
