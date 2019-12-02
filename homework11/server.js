b119671ae2993a5f20cbbe115e1822220fd9e4c8
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2019-12-02 16:10:11 -0600

    created express routes

--- homework11/server.js
+++ homework11/server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var path = require("path");
-
+var fs = require("fs")
 var app = express();
 var PORT = process.env.PORT || 3001;
-
+var notes = [];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -18,6 +18,20 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/view.html"));
+app.get("/api/notes", function(req, res) {
+    return res.json(notes);
+});
+
+app.get("/api/notes", function(req, res) {
+    res.json(fs.read(path.join(__dirname, "db/db.json")));
+});
+
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    notes.push(newNote);
+    res.json(newNote);
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
 });
